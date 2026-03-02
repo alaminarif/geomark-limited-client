18a856632782e61956ace33d602cd47df8da0e55
Arifur Rahman <arifur3429@gmail.com>
2026-03-02 23:42:14 +0600

    projec filter done

---
+++ src/components/modules/Project/ProjectFilter.tsx
@@ -0,0 +1,65 @@
+import Loading from "@/components/layout/Loading";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { useGetAllProjectsQuery } from "@/redux/features/project/project.api";
+import { useGetAllServicesQuery } from "@/redux/features/service/service.api";
+import { useSearchParams } from "react-router";
+
+const ProjectFilter = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const { data: projects } = useGetAllProjectsQuery(undefined);
+  const { data: services, isLoading } = useGetAllServicesQuery(undefined);
+
+  const seletedService = searchParams.get("name") || undefined;
+
+  const serviceOptions = services?.data?.map((item: { _id: string; name: string }) => ({
+    label: item.name,
+    value: item._id,
+  }));
+
+  console.log(serviceOptions);
+
+  const handleServiceChange = (value: string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("name", value);
+    setSearchParams(params);
+    console.log(value);
+  };
+
+  const handleClearFilter = () => {
+    const params = new URLSearchParams(searchParams);
+    params.delete("name");
+    // params.delete("tourType");
+    setSearchParams(params);
+  };
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <div>
+      <div className="div">
+        <div className="div">
+          <Select onValueChange={handleServiceChange} value={seletedService ? seletedService : ""} disabled={isLoading}>
+            <SelectTrigger className="w-full max-w-48">
+              <SelectValue placeholder="Select A Category" />
+            </SelectTrigger>
+            <SelectContent className="">
+              <SelectGroup>
+                <SelectLabel>Category</SelectLabel>
+                {serviceOptions?.map((item: { value: string; label: string }) => (
+                  <SelectItem key={item.value} value={item.value} className="text-white">
+                    {item.label}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectFilter;
--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,20 +1,36 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import Loading from "@/components/layout/Loading";
+import ProjectFilter from "@/components/modules/Project/ProjectFilter";
 import { useGetAllProjectsQuery } from "@/redux/features/project/project.api";
-import { useGetAllServicesQuery } from "@/redux/features/service/service.api";
-// import { useNavigate } from "react-router";
+import { useSearchParams } from "react-router";
 
 const ProjectPage = () => {
   // const Navigate = useNavigate();
+  const [searchParams] = useSearchParams();
 
-  const { data: projects } = useGetAllProjectsQuery(undefined);
-  const { data, isLoading } = useGetAllServicesQuery(undefined);
-  const services = data?.data || [];
+  const service = searchParams.get("name") || undefined;
 
-  if (isLoading) return <Loading />;
+  const { data: projects, isLoading } = useGetAllProjectsQuery({ service });
+  // const TServiceOtions =
+
+  // console.log("servieOption", serviceOptions);
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <section className="container mx-auto">
-      <div></div>
+      <div>
+        <div className="">
+          <ProjectFilter />
+        </div>
+        {projects?.data.map((project: any) => (
+          <div className=" my-6">
+            <p> {project.title}</p>
+            <p>{project.name}</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
