7b34f8662eaf7a271ccc5e753b3578f1dcde336f
Arifur Rahman <arifur3429@gmail.com>
2026-03-03 22:56:08 +0600

    projec pagination done

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,93 +1,5 @@
-// /* eslint-disable @typescript-eslint/no-explicit-any */
-// import Loading from "@/components/layout/Loading";
-// import ProjectFilter from "@/components/modules/Project/ProjectFilter";
-// import { useGetAllProjectsQuery } from "@/redux/features/project/project.api";
-// import { useNavigate, useSearchParams } from "react-router";
-// import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-// import { Button } from "@/components/ui/button";
-// import { format } from "date-fns";
-
-// const ProjectPage = () => {
-//   const navigate = useNavigate();
-//   const [searchParams] = useSearchParams();
-
-//   const title = searchParams.get("title") || undefined;
-//   const status = searchParams.get("status") || undefined;
-
-//   const { data: projects, isLoading } = useGetAllProjectsQuery({ title, status });
-//   // const TServiceOtions =
-
-//   // console.log("servieOption", serviceOptions);
-//   if (isLoading) {
-//     return <Loading />;
-//   }
-//   const handleProjectDetails = (id: string) => {
-//     navigate(`/project/${id}`);
-//   };
-//   return (
-//     <section className="container mx-auto">
-//       <div>
-//         <div className="">
-//           <ProjectFilter />
-//         </div>
-//         <Table className="border border-muted rounded-4xl mt-6">
-//           <TableHeader>
-//             <TableRow>
-//               <TableHead className="text-center font-bold">Image</TableHead>
-//               <TableHead className="text-center font-bold ">Project Name</TableHead>
-//               <TableHead className="text-center font-bold">Period</TableHead>
-//               <TableHead className="text-center font-bold">Status</TableHead>
-//               <TableHead className="text-center font-bold">Sector</TableHead>
-//               <TableHead className="text-center font-bold">Client</TableHead>
-//               <TableHead className="text-right font-bold">Action</TableHead>
-//             </TableRow>
-//           </TableHeader>
-
-//           <TableBody>
-//             {projects?.data?.map((item: any) => (
-//               <TableRow className="">
-//                 <TableCell className="font-medium">
-//                   <div className="">
-//                     <img src={item.picture} className="w-25 h-20 rounded-xl" />
-//                   </div>
-//                 </TableCell>
-
-//                 <TableCell className="font-medium">
-//                   <p className="font-medium whitespace-normal wrap-break-word">{item.name}</p>
-//                 </TableCell>
-
-//                 <TableCell className="">
-//                   <p>{item?.startDate ? format(new Date(item.startDate), "PPP") : "-"}</p>
-//                 </TableCell>
-
-//                 <TableCell className="">
-//                   <p>{item.status}</p>
-//                 </TableCell>
-
-//                 <TableCell className="">
-//                   <p className=" whitespace-normal wrap-break-word">{item.title}</p>
-//                 </TableCell>
-
-//                 <TableCell className="">
-//                   <p>{item?.client?.name}</p>
-//                 </TableCell>
-//                 <TableCell className="font-medium text-right">
-//                   <Button onClick={() => handleProjectDetails(item?._id)} variant="outline" className="text-destructive">
-//                     View
-//                   </Button>
-//                 </TableCell>
-//               </TableRow>
-//             ))}
-//           </TableBody>
-//         </Table>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default ProjectPage;
-
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { Pagination, PaginationContent, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/pagination";
 import Loading from "@/components/layout/Loading";
 import ProjectFilter from "@/components/modules/Project/ProjectFilter";
 import { useGetAllProjectsQuery } from "@/redux/features/project/project.api";
@@ -102,14 +14,18 @@ type ViewMode = "card" | "table";
 
 const ProjectPage = () => {
   const navigate = useNavigate();
+  const [viewMode, setViewMode] = useState<ViewMode>("table");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [limit] = useState(3);
   const [searchParams] = useSearchParams();
 
   const title = searchParams.get("title") || undefined;
   const status = searchParams.get("status") || undefined;
 
-  const { data: projects, isLoading } = useGetAllProjectsQuery({ title, status });
+  const { data: projects, isLoading } = useGetAllProjectsQuery({ title, status, page: currentPage, limit });
 
-  const [viewMode, setViewMode] = useState<ViewMode>("table");
+  const totalPage = projects?.meta?.totalPage || 1;
+  console.log(totalPage);
 
   if (isLoading) return <Loading />;
 
@@ -171,7 +87,7 @@ const ProjectPage = () => {
                   </TableCell>
 
                   <TableCell className="font-medium">
-                    <p className="font-medium whitespace-normal break-words">{item.name}</p>
+                    <p className="font-medium whitespace-normal wrap-break-word">{item.name}</p>
                   </TableCell>
 
                   <TableCell>
@@ -183,7 +99,7 @@ const ProjectPage = () => {
                   </TableCell>
 
                   <TableCell>
-                    <p className="whitespace-normal break-words">{item.title}</p>
+                    <p className="whitespace-normal wrap-break-word">{item.title}</p>
                   </TableCell>
 
                   <TableCell>
@@ -199,6 +115,34 @@ const ProjectPage = () => {
               ))}
             </TableBody>
           </Table>
+        )}
+
+        {totalPage > 1 && (
+          <div className="flex justify-end mt-4">
+            <div>
+              <Pagination>
+                <PaginationContent>
+                  <PaginationItem>
+                    <PaginationPrevious
+                      onClick={() => setCurrentPage((prev) => prev - 1)}
+                      className={currentPage === 1 ? "pointer-events-none opacity-50" : "cursor-pointer"}
+                    />
+                  </PaginationItem>
+                  {Array.from({ length: totalPage }, (_, index) => index + 1).map((page) => (
+                    <PaginationItem key={page} onClick={() => setCurrentPage(page)}>
+                      <PaginationLink isActive={currentPage === page}>{page}</PaginationLink>
+                    </PaginationItem>
+                  ))}
+                  <PaginationItem>
+                    <PaginationNext
+                      onClick={() => setCurrentPage((prev) => prev + 1)}
+                      className={currentPage === totalPage ? "pointer-events-none opacity-50" : "cursor-pointer"}
+                    />
+                  </PaginationItem>
+                </PaginationContent>
+              </Pagination>
+            </div>
+          </div>
         )}
       </div>
     </section>
